711c577261d108980e0929a33f4f863f94819934
sunilsingh77 <sunilsingh77@gmail.com>
2023-12-15 00:11:26 +0530

    added generic function

---
+++ Angular_DotNetCore_JWTAuth_RT/CoreAngularAppWithJWTAuth/Extensions/EFStoredProcedureCall.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+namespace CoreAngularAppWithJWTAuth.Extensions
+{
+    public static class EFSqlHelper
+    {
+        //var result = Context.ExecuteQuery<EvidenceReportInfo>(CommonFunctions.ExecuteSpForEvidenceReport(evidenceId, filterText));
+        public static List<T> ExecuteQuery<T>(this DbContext db, string query) where T : class, new()
+        {
+            using (var command = db.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = query;
+                command.CommandType = CommandType.Text;
+
+                db.Database.OpenConnection();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    var lst = new List<T>();
+                    var lstColumns = new T().GetType().GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).ToList();
+                    while (reader.Read())
+                    {
+                        var newObject = new T();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            var name = reader.GetName(i);
+                            var prop = lstColumns.FirstOrDefault(a => a.Name.ToLower().Equals(name.ToLower()));
+                            if (prop == null)
+                            {
+                                continue;
+                            }
+                            var val = reader.IsDBNull(i) ? null : reader[i];
+                            prop.SetValue(newObject, val, null);
+                        }
+                        lst.Add(newObject);
+                    }
+
+                    return lst;
+                }
+            }
+        }
+    }
+}
